e6c9dd25bd288e1ddcb031d84ad8c95a81b4f6d3
Roman Frigg <frigg@kooaba.com>
2013-08-22 10:53:29 +0200

    split python api (api, example), correct logging

--- python/KooabaApi.py
+++ python/KooabaApi.py
@@ -1,5 +1,3 @@
-
-
 from StringIO import StringIO
 from textwrap import dedent
 from urlparse import urlparse
@@ -8,71 +6,62 @@ import httplib
 import mimetypes
 import os
 import json
-import traceback
+import logging
 
 from KASignature import KASignature
 
-
-VERSION = '1.1.0'
-
+version = '1.2.0'
 
 # Configuration 
-UPLOAD_ENDPOINT = 'https://upload-api.kooaba.com/'
-BUCKET_ID = '<enter-bucket-id>' 
-
-DATA_KEY_SECRET_TOKEN = '<enter-secret-token>'
 
 # with KA auth, both http and https are possible
+UPLOAD_ENDPOINT = 'https://upload-api.kooaba.com/'
 QUERY_ENDPOINT= 'https://query-api.kooaba.com/v4/query'
 
-QUERY_KEY_SECRET_TOKEN = '<enter-secret-token>' 
-QUERY_KEY_ID='<enter-key-id>' ## only needed for KA authentication
-
-
+logger = logging.getLogger(__name__)
 
 class BasicAPIClient:
     """ Client for kooaba  API V4. """
 
     def __init__(self, secret_token, key_id=None):
         self.KA = KASignature(secret_token)
-        self.key_id=key_id
-        self.secret_token=secret_token
+        self.key_id = key_id
+        self.secret_token = secret_token
 
 
     #### QUERY API   
 
     def query(self,filename, auth_method='Token'):
         data, content_type = self.data_from_file(filename)
-        content_type, body=self.encode_multipart_formdata([],[('image', filename, data)])
+        content_type, body = self.encode_multipart_formdata([],[('image', filename, data)])
 
         (response, body) = self._send_request('POST', QUERY_ENDPOINT, bytearray(body), content_type, auth_method)
         return json.loads(body)
-    
 
 
     #### UPLOAD API (subset of available methods)
 
     def create_item(self, bucket_id, title, refid, json_string):
-        url=UPLOAD_ENDPOINT+'api/v4/buckets/'+bucket_id+'/items'
+        url = UPLOAD_ENDPOINT+'api/v4/buckets/'+bucket_id+'/items'
 
-        metadata=json.loads(json_string)
-        data= {"title":title, "reference_id":refid, "metadata":metadata}
+        metadata = json.loads(json_string)
+        data = {"title":title, "reference_id":refid, "metadata":metadata}
 
         (response, body) = self._send_request('POST', url, json.dumps(data), 'application/json')
         return json.loads(body)
 
 
     def attach_image(self, bucket_id, item_id, content_type, data):
-        url=UPLOAD_ENDPOINT+'api/v4/items/'+item_id+'/images'
+        url = UPLOAD_ENDPOINT+'api/v4/items/'+item_id+'/images'
 
         (response, body) = self._send_request('POST', url, bytearray(data), content_type)
         return json.loads(body)
 
    
     def replace_metadata(self, item_id, json_string):
-        url=UPLOAD_ENDPOINT+'api/v4/items/'+item_id
-        metadata=json.loads(json_string)
-        data= {"metadata": metadata}
+        url = UPLOAD_ENDPOINT+'api/v4/items/'+item_id
+        metadata = json.loads(json_string)
+        data = {"metadata": metadata}
         (response, body) = self._send_request('PUT', url, json.dumps(data), 'application/json')
         return json.loads(body)
 
@@ -97,13 +86,13 @@ class BasicAPIClient:
             - RuntimeError: Unsupported transport scheme.
             - RuntimeError: API call returned an error.
         """
-        if True:
-            if data is None:
-               sys.stderr.write( " > " + method +' '+ api_path) 
-            elif len(data) < 4096:
-                print " > %s ...%s:\n > %s" % (method, api_path, data)
-            else:
-                print " > %s ...%s: %sB" % (method, api_path, len(data))
+
+        if data is None:
+            logger.info("> %s %s", method, api_path) 
+        elif len(data) < 4096:
+            logger.info("> %s %s: > %s", method, api_path, data)
+        else:
+            logger.info("> %s %s: %sB", method, api_path, len(data))
         
         parsed_url = urlparse(api_path)
         
@@ -112,7 +101,7 @@ class BasicAPIClient:
        
         port = parsed_url.port
         if port is None:
-            port=80 
+            port = 80 
             if (parsed_url.scheme == 'https'):
                port = 443
 
@@ -120,10 +109,8 @@ class BasicAPIClient:
 
         if (parsed_url.scheme == 'https'):
             http = httplib.HTTPSConnection(host, port )
-        
         elif (parsed_url.scheme == 'http'):
             http = httplib.HTTPConnection(host, port )
-
         else:
             raise RuntimeError("URL scheme '%s' not supported" % parsed_url.scheme)
         
@@ -133,27 +120,30 @@ class BasicAPIClient:
             if auth_method=='KA':
                 signature = self.KA.sign(method, data, content_type, date, parsed_url.path)
                 headers = {'Authorization': 'KA %s:%s' % (self.key_id,signature),'Date': date}
-                print "signature: "+headers['Authorization']
-
+                logger.info("signature: %s", headers['Authorization'])
             else: # Token
                 headers = {'Authorization': 'Token %s' % (self.secret_token),'Date': date}
 
             if content_type is not None:
                 headers['Content-Type'] = content_type
+
             if data is not None:
                 headers['Content-Length'] = str(len(data))
+
             try:
                 http.request(method, parsed_url.path, body=data,  headers=headers)
             except Exception, e:
                 raise  #IOError("Error during request: %s: %s" % (type(e), e))
+
             response = http.getresponse()
             # we have to read the response before the http connection is closed
             body = response.read()
-            if True:
-                sys.stderr.write( " < " + str(response.status) + ' ' + str(response.reason)+'\n')
-                sys.stderr.write( " < " + body+'\n')
+            logger.info("< %d %s", response.status, response.reason)
+            logger.info("< %s", body)
+
             if (response.status < 200) or (response.status > 299):
                 raise RuntimeError("API call returned status %s %s. Message: %s" % (response.status, response.reason, body))
+
             return (response, body)
         finally:
             http.close()
@@ -165,6 +155,7 @@ class BasicAPIClient:
         files is a sequence of (name, filename, value) elements for data to be uploaded as files
         Return (content_type, body) ready for httplib.HTTP instance
         """
+
         BOUNDARY = '----------ThIs_Is_tHe_bouNdaRY_$'
         CRLF = '\r\n'
         L = []
@@ -187,53 +178,4 @@ class BasicAPIClient:
 
     def get_content_type(self, filename):
         return mimetypes.guess_type(filename)[0] or 'application/octet-stream' 
-
-
-### example usage of api
-def main():
-
-    query_example()
-    #upload_example()
-
-
-def query_example():
-
-    #simple example of KA signature
-    signer=KASignature('aSecretKey')
-    signature=signer.sign('POST', 'aBody','text/plain', 'Sun, 06 Nov 1994 08:49:37 GMT', '/v2/query')
-    print signature
-    print 'expected: '+'6XVAzB+hA9JEFWZdg+1ssZ+gfRo='
-    
-    # perform an actual request
-    client = BasicAPIClient(QUERY_KEY_SECRET_TOKEN, QUERY_KEY_ID)
-   
-    try:
-       result =client.query('../images/query_image.jpg', 'KA')
-
-    except:
-       print "call failed:" , sys.exc_info()
-       traceback.print_exc()
-
-
-def upload_example():
-
-    client = BasicAPIClient(DATA_KEY_SECRET_TOKEN)
-
-    # loading an image into memory
-    data, content_type = client.data_from_file('../images/db_image.jpg')
-    
-    try:
-       item =client.create_item(BUCKET_ID, 'aTitle', 'myRefId', '{}')
-       print 'created item '+item['uuid']
-       images=client.attach_image(BUCKET_ID, item['uuid'], content_type, data)
-       print 'attached image '+images[0]['sha1']
-    except:
-       print "Upload failed:", sys.exc_info()
-       traceback.print_exc()
-
-
-
-if __name__ == '__main__':
-    import sys
-    sys.exit(main())
 
---
+++ python/KooabaApi_demo.py
@@ -0,0 +1,60 @@
+import traceback
+import logging
+
+from KASignature import KASignature
+from KooabaApi import BasicAPIClient
+
+# Configuration 
+BUCKET_ID = '<enter bucket id' 
+DATA_KEY_SECRET_TOKEN = 'enter data api token'
+QUERY_KEY_SECRET_TOKEN = 'enter query api token' 
+
+## only needed for KA authentication
+QUERY_KEY_ID = 'enter query api key id' 
+
+# Use the INFO logging level and log all output to stdout (default)
+logging.basicConfig(level=logging.INFO, format='%(name)s:%(levelname)s: %(message)s')
+logger = logging.getLogger(__name__)
+
+### example usage of api
+def main():
+    query_example()
+    #upload_example()
+
+
+def query_example():
+    #simple example of KA signature
+    signer = KASignature('aSecretKey')
+    signature = signer.sign('POST', 'aBody','text/plain', 'Sun, 06 Nov 1994 08:49:37 GMT', '/v2/query')
+    logger.info('generated signature: %s', signature)
+    logger.info('expected signature: 6XVAzB+hA9JEFWZdg+1ssZ+gfRo=')
+    
+    # perform an actual request
+    client = BasicAPIClient(QUERY_KEY_SECRET_TOKEN, QUERY_KEY_ID)
+   
+    try:
+        result = client.query('../images/query_image.jpg', 'KA')
+    except:
+        logger.exception('Query failed')
+
+
+def upload_example():
+
+    client = BasicAPIClient(DATA_KEY_SECRET_TOKEN)
+
+    # loading an image into memory
+    data, content_type = client.data_from_file('../images/db_image.jpg')
+    
+    try:
+       item = client.create_item(BUCKET_ID, 'aTitle', 'myRefId', '{}')
+       logger.info('created item %s', item['uuid'])
+       images = client.attach_image(BUCKET_ID, item['uuid'], content_type, data)
+       logger.info('attached image %s', images[0]['sha1'])
+    except:
+       logger.exception('Upload failed')
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main())
+
